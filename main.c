3ead4ee131f15745a3ff32a8be88da268dcad670
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-26 21:14:02 +0300

    Task done

--- main.c
+++ main.c
@@ -70,13 +70,13 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file: %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	read_f = getline(&line, &line_size, file);
